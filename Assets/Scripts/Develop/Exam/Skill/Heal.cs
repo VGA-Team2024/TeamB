00b01dddce1850310d382cdf0be61e00574a00fc
Yusuke17 <yuusuke.k1717@gmail.com>
2024-11-28 09:06:04 +0900

    [add]回復機能追加

---
+++ Assets/Scripts/Develop/Exam/Skill/Heal.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System;
+using TeamB.Develop;
+using TeamB.GameSystem.Statics;
+
+namespace TeamB.Develop
+{
+    /// <summary>
+    ///     魔法攻撃力バフ
+    /// </summary>
+    [Serializable]
+    public class Heal : ISkill
+    {
+        [Tooltip("％表記で"), SerializeField] private float _value;
+        [SerializeField] private GameObject _button;
+        [SerializeField] BuffUI _buffUI;
+        public event Action OnChantingSkill;
+
+        public void Activation(ICharacter mainCharacter, ICharacter character)
+        {
+            character.TakeDamage(-GameStatics.Characters[(int)GameStatics.NurturingCharacterType].Hp / 100f * _value);
+            _button.SetActive(false);
+            _buffUI.ReturnBuffUI();
+        }
+    }
+}
